e485db129a6a7f53afc5e8688583ec5298ebff97
yannickjones <yannick.jones@ipsos.com>
2026-02-25 22:01:06 +0000

    tf fmt

--- modules/compute-instance/variables.tf
+++ modules/compute-instance/variables.tf
@@ -104,7 +104,7 @@ variable "network_ip" {
 }
 
 variable "boot_disk_name" {
-  type = string
-  default = null
+  type        = string
+  default     = null
   description = "Name of the boot disk to create. If null, a name will be generated based on the instance name"
 }
